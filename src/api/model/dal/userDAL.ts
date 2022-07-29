b50ef5c44834bdf49fd92e4bf78ca9ef2456bd4c
Fabio Quintal <faaquintal@gmail.com>
2022-07-29 15:35:24 -0300

    init db on different local enviroment

--- src/api/model/dal/userDAL.ts
+++ src/api/model/dal/userDAL.ts
@@ -1,9 +1,11 @@
 import User from '../user/User';
 
 import { UserInput, UserOuput } from '../../../types/user';
+import dbInit from '../init/init';
 
 class DAL {
 	async create(payload: UserInput): Promise<UserOuput> {
+		dbInit()
 		try {
 			const user = await User.create(payload);
 			return Promise.resolve(user);
